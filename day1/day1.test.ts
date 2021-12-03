5b1e419098f7858cdb5ff6c4d1ba729d6d5dca03
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2021-12-03 19:11:50 +0000

    Day one part two

--- day1/day1.test.ts
+++ day1/day1.test.ts
@@ -1,5 +1,4 @@
-import {partOne} from "./day1";
-import {countIncreases} from "./day1";
+import {partOne, partTwo, countIncreases, countWindowIncreases} from "./day1";
 describe('day1', () => {
     test('empty', () => {
         expect(countIncreases([])).toEqual(0)
@@ -17,5 +16,24 @@ describe('day1', () => {
         let partOneAnswer = partOne("day1/input.txt");
         expect(partOneAnswer).toBeGreaterThan(0);
         console.log(`Part one: ${partOneAnswer}`);
+    })
+    test('zero when not enough numbers', () => {
+        expect(countWindowIncreases([])).toEqual(0)
+        expect(countWindowIncreases([1, 2])).toEqual(0)
+        expect(countWindowIncreases([1, 2, 3])).toEqual(0)
+    })
+    test('window one increase', () => {
+        expect(countWindowIncreases([1, 2, 1, 2])).toEqual(1)
+    })
+    test('window two increases', () => {
+        expect(countWindowIncreases([1, 2, 1, 2, 3])).toEqual(2)
+        expect(countWindowIncreases([1, 1, 1, 1, 1, 2, 2])).toEqual(2)
+        expect(countWindowIncreases([1, 1, 1, 1, 1, 2, 1, 2])).toEqual(2)
+        expect(countWindowIncreases([1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, ])).toEqual(2)
+    })
+    test('part2', () => {
+        let partTwoAnswer = partTwo("day1/input.txt");
+        expect(partTwoAnswer).toBeGreaterThan(0);
+        console.log(`Part two: ${partTwoAnswer}`);
     })
 })
--- day1/day1.ts
+++ day1/day1.ts
@@ -3,8 +3,19 @@ import {readNumberArray} from "../reader";
 export const countIncreases = (numbers: number[]) => {
     let increases: number = 0;
     for (let index = 1; index < numbers.length; index++) {
-        if (numbers[index] > numbers[index-1]) {
+        if (numbers[index] > numbers[index - 1]) {
             increases++;
+        }
+    }
+    return increases
+}
+export const countWindowIncreases = (numbers: number[]) => {
+    let increases: number = 0;
+    if (numbers.length > 3) {
+        for (let index = 3; index < numbers.length; index++) {
+            if (numbers[index] + numbers[index-1] + numbers[index-2] > numbers[index - 1] + numbers[index - 2] + numbers[index - 3]) {
+                increases++;
+            }
         }
     }
     return increases
@@ -12,4 +23,8 @@ export const countIncreases = (numbers: number[]) => {
 
 export const partOne = (path: string) => {
     return countIncreases(readNumberArray(path));
+}
+
+export const partTwo = (path: string) => {
+    return countWindowIncreases(readNumberArray(path));
 } 
